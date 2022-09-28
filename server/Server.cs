00945d267f29d7b52369a2e862163fa050551b2c
samuel-barrows <samuel.barrows@mymail.champlain.edu>
2022-09-28 13:23:53 -0400

    changed how the server listens (now asynchronous)

--- server/Server.cs
+++ server/Server.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.IO;
+using System.Threading.Tasks;
 
 namespace server
 {
@@ -9,7 +10,8 @@ namespace server
             //take command line arguments of Name, Port, IPaddress
             Listener test = new Listener(args[0], Int32.Parse(args[1]), args[2]);
             test.StartServer();
-            test.AsyncRespond();
+            var handle = test.Listen();
+            handle.Wait();
             test.Terminate();
         }
     }
@@ -35,18 +37,20 @@ namespace server
         private HttpListener _listener;
 
         public Listener(string name, int port, string ipaddress){
-            Console.WriteLine("Constructing {0}",name);
             this.Name = name;
             this.Port = port;
             this.Ipaddress = ipaddress;
+
             this.path = $"data/listeners/{this.Name}/";
             this.keyPath = $"{this.path}key";
             this.filePath = $"{this.path}files/";
             this.agentsPath = $"{this.path}agents/";
+
             //Create the paths defined above if they don't already exist
             Directory.CreateDirectory(this.path);
             Directory.CreateDirectory(this.filePath);
             Directory.CreateDirectory(this.agentsPath);
+
             //will need to generate a key in this location
             File.Create(this.keyPath);
 
@@ -72,37 +76,46 @@ namespace server
             _listener.Stop();
         }
 
-        private void Listen(IAsyncResult result){
+        public async Task Listen(){
             //per https://learn.microsoft.com/en-us/dotnet/api/system.net.httplistener?view=net-6.0
-            HttpListener listener = (HttpListener) result.AsyncState;
-            // Call EndGetContext to complete the asynchronous operation.
-            HttpListenerContext context = listener.EndGetContext(result);
-            HttpListenerRequest request = context.Request;
-            // Obtain a response object.
-            HttpListenerResponse response = context.Response;
-            // Construct a response.
-            string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
-            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
-            // Get a response stream and write the response to it.
-            response.ContentLength64 = buffer.Length;
-            System.IO.Stream output = response.OutputStream;
-            output.Write(buffer,0,buffer.Length);
-            // You must close the output stream.
-            output.Close();
+            //and https://gist.github.com/define-private-public/d05bc52dd0bed1c4699d49e2737e80e7
+            while(true){
+                //Get our context asynchronously
+                HttpListenerContext context = await _listener.GetContextAsync();
+                HttpListenerRequest request = context.Request;
+                // Obtain a response object.
+                HttpListenerResponse response = context.Response;
+
+                // Construct a response.
+                string responseString = "<HTML><BODY> Hello world!</BODY></HTML>";
+                byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+                // Get a response stream and write the response to it.
+                response.ContentLength64 = buffer.Length;
+
+                //write our response string to the client using the outputstream
+                System.IO.Stream output = response.OutputStream;
+                output.Write(buffer,0,buffer.Length);
+
+                // You must close the output stream.
+                output.Close();
+            }
         }
 
         public void AsyncRespond(){
             //per https://learn.microsoft.com/en-us/dotnet/api/system.net.httplistener?view=net-6.0
             //loop 5 times
-            for (int x = 0; x < 5; x++){
+            while(true){
+                /*
                 IAsyncResult result = _listener.BeginGetContext(new AsyncCallback(Listen),_listener);
                 // Applications can do some work here while waiting for the
                 // request. If no work can be done until you have processed a request,
                 // use a wait handle to prevent this thread from terminating
                 // while the asynchronous operation completes.
+                result.StartAsync();
                 Console.WriteLine("Waiting for request to be processed asyncronously.");
-                result.AsyncWaitHandle.WaitOne();
-                Console.WriteLine("Request processed asyncronously.");
+                //Console.WriteLine("Request processed asyncronously.");
+                */
             }
         }
 
