90ddf3e99aac91cc8d76c256b3ca5d02ea423bb7
Damir Hajrovic <hajrovic.damir@gmail.com>
2022-10-26 16:27:00 -0400

    lol
    Fixes server to use default options.

--- server/Server.cs
+++ server/Server.cs
@@ -5,25 +5,36 @@ namespace server
     public class Server{
         static void Main(string[] args){
             Console.Clear();
+            //take command line arguments of Name, Port, IPaddress
+            int tport;
+            string tname;
+            string tipaddress;
             try {
-                //take command line arguments of Name, Port, IPaddress
-                Listener test = new Listener(args[0], Int32.Parse(args[1]), args[2]);
-                UserInput input = new UserInput(test);
-                test.StartServer();
-                //load already registered clients
-                test.LoadRegisteredClients();
-                //start the async listener
-                //meant to not be awaited so that it runs in the background
-                test.ListenAndRespond();
-                //start our menu and take server input
-                //acts as a block for the async functions
-                input.MenuInput();
-                //stop the server
-                test.Terminate();
+                tname = args[0];
+                tport = Int32.Parse(args[1]);
+                tipaddress = args[2];
+                //Listener test = new Listener(args[0], tport, args[2]);
             } catch (System.IndexOutOfRangeException) {
-                Console.WriteLine("Please provide the name of the listener, port to listen on, and the IP to bind to.");
-                Environment.Exit(0);
+                tport = 1234;
+                tname = "Default";
+                tipaddress = "127.0.0.1";
+                
+                Console.WriteLine("No arguments provided, using defaults...");
             }
+
+            Listener test = new Listener(tname, tport, tipaddress);
+            UserInput input = new UserInput(test);
+            test.StartServer();
+            //load already registered clients
+            test.LoadRegisteredClients();
+            //start the async listener
+            //meant to not be awaited so that it runs in the background
+            test.ListenAndRespond();
+            //start our menu and take server input
+            //acts as a block for the async functions
+            input.MenuInput();
+            //stop the server
+            test.Terminate();
         }
     }
 
